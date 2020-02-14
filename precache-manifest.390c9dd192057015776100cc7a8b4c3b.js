7509dc425f5a6c86049abb3cc4958f4b04fde408
Catalina Campos <catalina.camposcaceres9@gmail.com>
2020-02-14 19:45:47 -0300

    Updates

---
+++ precache-manifest.390c9dd192057015776100cc7a8b4c3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a80218c2cd290ba468dc250ef800ce",
+    "url": "/CatalinaCampos/muestra-app/index.html"
+  },
+  {
+    "revision": "1f0676e8486cf7833e79",
+    "url": "/CatalinaCampos/muestra-app/static/css/main.06a32b52.chunk.css"
+  },
+  {
+    "revision": "78e229c769641253824c",
+    "url": "/CatalinaCampos/muestra-app/static/js/2.09bbabc0.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/CatalinaCampos/muestra-app/static/js/2.09bbabc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0676e8486cf7833e79",
+    "url": "/CatalinaCampos/muestra-app/static/js/main.4ae13431.chunk.js"
+  },
+  {
+    "revision": "cdf4aa9cc558868b66bf",
+    "url": "/CatalinaCampos/muestra-app/static/js/runtime-main.f1700db4.js"
+  },
+  {
+    "revision": "bf02a7601170733ad5dbeb8390c398d4",
+    "url": "/CatalinaCampos/muestra-app/static/media/code.bf02a760.jpg"
+  },
+  {
+    "revision": "deeabcde23c13a56a4fcc8571638986f",
+    "url": "/CatalinaCampos/muestra-app/static/media/company.deeabcde.jpg"
+  },
+  {
+    "revision": "547379c7c2ff335d43213e7e2ad2fb37",
+    "url": "/CatalinaCampos/muestra-app/static/media/foto4.547379c7.jpg"
+  },
+  {
+    "revision": "4bc33a92442c4009d0ea8ae8861c4ff6",
+    "url": "/CatalinaCampos/muestra-app/static/media/meeting.4bc33a92.jpg"
+  },
+  {
+    "revision": "eff9acc726a4a0fe0ed8d750a8058ab1",
+    "url": "/CatalinaCampos/muestra-app/static/media/ux.eff9acc7.jpg"
+  }
+]);
