5e466f2ac3238be4957e3cd46eb3e68d58144639
Catalina Campos <catalina.camposcaceres9@gmail.com>
2020-02-14 20:00:19 -0300

    Updates

---
+++ precache-manifest.9db9b724ececbf1df4a6beab2833e27e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a81818d660a4c95d8181397412d7cd",
+    "url": "/muestra-app/index.html"
+  },
+  {
+    "revision": "cba0d3d75db28de5f48b",
+    "url": "/muestra-app/static/css/main.06a32b52.chunk.css"
+  },
+  {
+    "revision": "78e229c769641253824c",
+    "url": "/muestra-app/static/js/2.09bbabc0.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/muestra-app/static/js/2.09bbabc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba0d3d75db28de5f48b",
+    "url": "/muestra-app/static/js/main.e7818b96.chunk.js"
+  },
+  {
+    "revision": "a0035e3fd8abe29c7360",
+    "url": "/muestra-app/static/js/runtime-main.6601a9f4.js"
+  },
+  {
+    "revision": "bf02a7601170733ad5dbeb8390c398d4",
+    "url": "/muestra-app/static/media/code.bf02a760.jpg"
+  },
+  {
+    "revision": "deeabcde23c13a56a4fcc8571638986f",
+    "url": "/muestra-app/static/media/company.deeabcde.jpg"
+  },
+  {
+    "revision": "547379c7c2ff335d43213e7e2ad2fb37",
+    "url": "/muestra-app/static/media/foto4.547379c7.jpg"
+  },
+  {
+    "revision": "4bc33a92442c4009d0ea8ae8861c4ff6",
+    "url": "/muestra-app/static/media/meeting.4bc33a92.jpg"
+  },
+  {
+    "revision": "eff9acc726a4a0fe0ed8d750a8058ab1",
+    "url": "/muestra-app/static/media/ux.eff9acc7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CatalinaCampos/muestra-app/precache-manifest.390c9dd192057015776100cc7a8b4c3b.js"
+  "/muestra-app/precache-manifest.9db9b724ececbf1df4a6beab2833e27e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CatalinaCampos/muestra-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/muestra-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
